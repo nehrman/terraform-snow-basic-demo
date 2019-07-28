d5165c772269b1d1fcb1f98c31be43297bf0e2fc
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2019-07-28 10:37:32 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+//--------------------------------------------------------------------
+// Modules
+module "id" {
+  source  = "app.terraform.io/Hashicorp-neh-snow-demo/id/random"
+  version = "0.1.1"
+
+  titi_length = 16
+  toto_length = 8
+}
