48d4ede4a24ebda695607a05a1d78b2009f0967d
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2019-07-28 19:27:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ module "id" {
   source  = "app.terraform.io/neh-snow-demo/id/random"
   version = "0.1.1"
 
-  titi_length = 16
-  toto_length = 8
+  titi_length = "${var.titi_length}"
+  toto_length = "${var.toto_length}"
 }
